6d2378b510f60dff9c1ad53c91eb8a2ebd7103b2
Nariety <narietyben@gmail.com>
2019-11-01 13:02:20 -0400

    face star iteration two, having issue with maintaining two heaps for both the blocks and the faces. Cannot properly store the connections between faces because it is not used in searching

--- maddux/robots/faceStar.py
+++ maddux/robots/faceStar.py
@@ -6,6 +6,7 @@
 import numpy as np
 import heapq
 
+blockWidth == 0.5
 
 class Block:
 
@@ -62,31 +63,48 @@ class PathPlanner:
     def heuristic(self, a, b):
         return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2 + (b[2] - a[2]) ** 2)
 
-    def astar(self, array, start, goal):
+    def astar(self, array, start, goal, startFace, goalFace):
 
-        neighbors = [(0, 1, 0), (0, 1, -1), (0, 1, 1),
-                     (0, -1, 0), (0, -1, -1), (0, -1, 1),
-                     (1, 0, 0), (1, 0, -1), (1, 0, 1),
-                     (-1, 0, 0), (-1, 0, -1), (-1, 0, 1),
-                     (1, 1, 0), (1, 1, -1), (1, 1, 1),
-                     (1, -1, 0), (1, -1, -1), (1, -1, 1),
-                     (-1, 1, 0), (-1, 1, -1), (-1, 1, 1),
-                     (-1, -1, 0), (-1, -1, -1), (-1, -1, 1)
+        # neighbors = [(0, 1, 0), (0, 1, -1), (0, 1, 1),
+        #              (0, -1, 0), (0, -1, -1), (0, -1, 1),
+        #              (1, 0, 0), (1, 0, -1), (1, 0, 1),
+        #              (-1, 0, 0), (-1, 0, -1), (-1, 0, 1),
+        #              (0, 0, 1), (0, 0, -1),
+        #              (1, 1, 0), (1, 1, -1), (1, 1, 1),
+        #              (1, -1, 0), (1, -1, -1), (1, -1, 1),
+        #              (-1, 1, 0), (-1, 1, -1), (-1, 1, 1),
+        #              (-1, -1, 0), (-1, -1, -1), (-1, -1, 1)
+        #              ]
+
+        neighbors = [(0, 1, 0),
+                     (0, -1, 0),
+                     (1, 0, 0),
+                     (-1, 0, 0),
+                     (0, 0, 1), 
+                     (0, 0, -1),
                      ]
 
         close_set = set()
         came_from = {}
         gscore = {start: 0}
-        fscore = {start: self.heuristic(start, goal)}
+        fscore = {start: self.heuristic(start, goalFace)}
         oheap = []
+
+        gscore[startFace] = 0
+        fscore[startFace] = self.heuristic(startFace,goalFace)
+        faceHeap = []
 
         heapq.heappush(oheap, (fscore[start], start))
 
+        # this implementation fails when the goal face cannot be directly reached
+        heapq.heappush(faceHeap, (fscore[startFace], startFace))
+
         while oheap:
             current = heapq.heappop(oheap)[1]
+            currentFace = heapq.heappop(faceHeap)[1]
             if current == goal:
                 data = []
-                while current in came_from:
+                while currentFace in came_from:
                     data.append(current)
                     current = came_from[current]
                 return data
@@ -95,6 +113,47 @@ class PathPlanner:
             for i, j, k in neighbors:
                 neighbor = current[0] + i, current[1] + j, current[2] + k
                 tentative_g_score = gscore[current] + self.heuristic(current, neighbor)
+                # for each available neighbor
+                if 0 <= neighbor[0] < array.shape[0]:
+                    if 0 <= neighbor[1] < array.shape[1]:
+                        if 0 <= neighbor[2] < array.shape[2]:
+                            if array[neighbor[0]][neighbor[1]][neighbor[2]] == 0:
+                                if i == 1: # face d
+                                    pass
+                                elif i == -1: # face c
+                                    pass
+                                elif j == 1: # face a
+                                    pass
+                                elif j == -1: # face b
+                                    pass
+                                elif k == 1: # face e
+                                    pass
+                                elif k == -1: # face f
+                                    pass
+                                neighborFace = current[0] + i*blockWidth, current[1] + j*blockWidth, current[2] + k*blockWidth
+                                # add face to heap when there is no neighbor on one side
+                                # TODO might be wrong to calculate g and f scores using the block itself instead of the actual face
+                                tentative_face_g_score = gscore[current] + self.heuristic(current, neighborFace)
+                                if tentative_face_g_score < gscore.get(neighborFace, 0) or neighborFace not in [i[1]for i in faceHeap]:
+                                    came_from[neighbor] = current
+                                    gscore[neighbor] = tentative_g_score
+                                    fscore[neighbor] = tentative_g_score + \
+                                        self.heuristic(neighbor, goalFace)
+                                    heapq.heappush(oheap, (fscore[neighbor], neighbor))
+                                continue
+                        else:
+                            continue
+                    else:
+                        # array bound y walls
+                        continue
+                else:
+                    # array bound x walls
+                    continue
+
+                if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                    continue
+                
+                # for each face on the block
                 if 0 <= neighbor[0] < array.shape[0]:
                     if 0 <= neighbor[1] < array.shape[1]:
                         if 0 <= neighbor[2] < array.shape[2]:
@@ -109,16 +168,12 @@ class PathPlanner:
                     # array bound x walls
                     continue
 
-                if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
-                    continue
-
                 if tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
                     came_from[neighbor] = current
                     gscore[neighbor] = tentative_g_score
                     fscore[neighbor] = tentative_g_score + \
-                        self.heuristic(neighbor, goal)
+                        self.heuristic(neighbor, goalFace)
                     heapq.heappush(oheap, (fscore[neighbor], neighbor))
-
 
     def get_path(self):
         route = self.astar(self.blueprint, self.start, self.goal)
@@ -139,17 +194,6 @@ class PathPlanner:
         return self.colors
 
     def get_traversal(self):
-        availableFaces = [
-            [1,1,0,0,0,0],
-            [0,0,0,0,1,1],
-            [1,1,0,0,0,0],
-            [0,0,0,0,1,1],
-            [1,1,0,0,0,0],
-            [0,0,0,0,1,1],
-            [1,1,0,0,0,0],
-            [0,0,0,0,1,1],
-        ]
-
         route = self.get_path()
         for i in range(len(route)):
             block = route[i]
